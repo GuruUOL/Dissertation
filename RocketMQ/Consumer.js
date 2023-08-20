8f6cf4ed2c520e1f53e0a11c09cad66ad7ee024c
GuruUOL <142727875+GuruUOL@users.noreply.github.com>
2023-08-20 21:06:41 +0100

    Update Consumer.js

--- RocketMQ/Consumer.js
+++ RocketMQ/Consumer.js
@@ -1,29 +1,39 @@
-const rocketmq = require('rocketmq-nodejs-client');
-const { PerformanceObserver, performance } = require('perf_hooks');
+const RocketMQConsumer = require('apache-rocketmq').Consumer;
 
-const consumer = new rocketmq.PushConsumer({
-  consumerGroup: 'test_group', // Replace with your consumer group
-  namesrv: 'localhost:9876' // Replace with your RocketMQ namesrv address
+const consumer = new RocketMQConsumer({
+  consumerGroup: 'test_consumer_group',
+  nameServer: 'localhost:9876' // Replace with your RocketMQ namesrv address
 });
 
-const observer = new PerformanceObserver((list) => {
-  const entry = list.getEntries()[0];
-  const latency = entry.duration;
-  console.log(`Received message | Latency: ${latency} ms`);
+let receivedMessages = 0;
+let startTime = Date.now();
+
+async function subscribeAndConsume() {
+  await consumer.subscribe({
+    topic: 'test_topic',
+    onMessage: async (message) => {
+      const startTimestamp = message.properties.startDeliverTime; // Start timestamp provided by RocketMQ
+      
+      // Process the message
+      
+      const endTimestamp = Date.now();
+      const latency = endTimestamp - startTimestamp;
+      console.log(`Received message | Latency: ${latency} ms`);
+
+      receivedMessages++;
+      if (receivedMessages % 100 === 0) {
+        const elapsedTime = (Date.now() - startTime) / 1000; // seconds
+        const throughput = receivedMessages / elapsedTime;
+        console.log(`Throughput: ${throughput.toFixed(2)} messages/s`);
+      }
+
+      await message.ack();
+    }
+  });
+
+  await consumer.start();
+}
+
+subscribeAndConsume().catch((err) => {
+  console.error('Error occurred:', err);
 });
-observer.observe({ entryTypes: ['measure'] });
-
-consumer.on('message', async (message) => {
-  const start = performance.now();
-
-  // Process the message
-
-  const end = performance.now();
-  performance.measure('message-received', start, end);
-
-  consumer.ack(message);
-});
-
-consumer.subscribe('test_topic'); // Replace with your topic
-
-consumer.start();
